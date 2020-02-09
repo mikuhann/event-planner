c2a7d130aa8602cfa8ff68cb386f6e31c7cd2075
mikuhann <ng.kano@gmail.com>
2020-02-09 22:52:11 +0300

    add form control

--- src/components/Event/EventForm/EventForm.js
+++ src/components/Event/EventForm/EventForm.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 
 import FormInput from './FormInput';
 
 const EventForm = ({closeForm}) => {
+  const [event, setEvent] = useState({
+    title: '',
+    date: '',
+    city: '',
+    venue: '',
+    hostedBy: '',
+  });
+
+  const { title, date, city, venue, hostedBy } = event;
+
+  const onChangeHandler = ({ target: { name, value } }) => setEvent({ ...event, [name]: value });
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(event)
+  };
   return (
     <Segment>
-      <Form>
-        <FormInput label='Event Title' placeholder='Event Title' />
-        <FormInput label='Event Date' type='date' placeholder='Event Date' />
-        <FormInput label='City' placeholder='City event is taking place' />
-        <FormInput label='Venue' placeholder='Enter the Venue of the event'/>
-        <FormInput label='Hosted by' placeholder='Enter the name of person hosting' />
+      <Form onSubmit={onSubmitHandler}>
+        <FormInput
+          label='Event Title'
+          name='title'
+          value={title}
+          onChange={onChangeHandler}
+          placeholder='Event Title'
+        />
+        <FormInput
+          label='Event Date'
+          name='date'
+          value={date}
+          type='date'
+          onChange={onChangeHandler}
+          placeholder='Event Date'
+        />
+        <FormInput
+          label='City'
+          name='city'
+          value={city}
+          onChange={onChangeHandler}
+          placeholder='City event is taking place'
+        />
+        <FormInput
+          label='Venue'
+          name='venue'
+          value={venue}
+          onChange={onChangeHandler}
+          placeholder='Enter the Venue of the event'
+        />
+        <FormInput
+          label='Hosted by'
+          name='hostedBy'
+          value={hostedBy}
+          onChange={onChangeHandler}
+          placeholder='Enter the name of person hosting'
+        />
         <Button positive type='submit' content='Submit' />
         <Button type='button' content='Cancel' onClick={closeForm} />
       </Form>
