59c827e8b455a673145d039de8648cb7c380f663
Mikuhann <ng.kano@gmail.com>
2020-03-02 22:19:13 +0300

    init redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import EventDashboard from './components/Event/EventDashboard';
 import NavBar from './components/Nav/';
@@ -12,29 +13,32 @@ import DashboardSettingsPage from './components/Users/Settings/DashboardSettings
 import EventForm from './components/Event/EventForm';
 
 import { Routes } from './constants/Routes';
+import store from './redux/store';
 
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
-    <>
-      <Route exact path={ Routes.HOME } component={ HomePage } />
-      <Route path='/(.+)' render={() => (
-        <>
-          <NavBar />
-          <Container className='main'>
-            <Switch>
-              <Route path={ Routes.EVENTS } component={ EventDashboard } />
-              <Route path={ Routes.EVENT } component={ EventDetailsPage } />
-              <Route path={ Routes.PROFILES } component={ UsersDashboardPage } />
-              <Route path={ Routes.PROFILE } component={ UserDetailsPage } />
-              <Route path={ Routes.SETTINGS } component={ DashboardSettingsPage } />
-              <Route path={ Routes.CREATE_EVENT } component={ EventForm } />
-            </Switch>
-          </Container>
-        </>
-      )} />
-    </>
+    <Provider store={store}>
+      <>
+        <Route exact path={ Routes.HOME } component={ HomePage } />
+        <Route path='/(.+)' render={() => (
+          <>
+            <NavBar />
+            <Container className='main'>
+              <Switch>
+                <Route path={ Routes.EVENTS } component={ EventDashboard } />
+                <Route path={ Routes.EVENT } component={ EventDetailsPage } />
+                <Route path={ Routes.PROFILES } component={ UsersDashboardPage } />
+                <Route path={ Routes.PROFILE } component={ UserDetailsPage } />
+                <Route path={ Routes.SETTINGS } component={ DashboardSettingsPage } />
+                <Route path={ Routes.CREATE_EVENT } component={ EventForm } />
+              </Switch>
+            </Container>
+          </>
+        )} />
+      </>
+    </Provider>
   );
 }
 
