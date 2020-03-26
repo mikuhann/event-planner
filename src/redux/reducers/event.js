4ec63edde640f35419bcddf94084dc48c9c97363
Mikuhann <ng.kano@gmail.com>
2020-03-26 23:56:31 +0300

    add scuffold event reducer

---
+++ src/redux/reducers/event.js
@@ -0,0 +1,86 @@
+import { CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT } from '../../constants/actions';
+
+const initialState = {
+  events: [
+    {
+      id: '1',
+      title: 'Trip to Tower of London',
+      date: '2018-03-27',
+      category: 'culture',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+      city: 'London, UK',
+      venue: `Tower of London, St Katharine's & Wapping, London`,
+      hostedBy: 'Bob',
+      hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+      attendees: [
+        {
+          id: 'a',
+          name: 'Bob',
+          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        },
+        {
+          id: 'b',
+          name: 'Tom',
+          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+        },
+      ]
+    },
+    {
+      id: '2',
+      title: 'Trip to Punch and Judy Pub',
+      date: '2018-03-28',
+      category: 'drinks',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+      city: 'London, UK',
+      venue: 'Punch & Judy, Henrietta Street, London, UK',
+      hostedBy: 'Tom',
+      hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      attendees: [
+        {
+          id: 'b',
+          name: 'Tom',
+          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+        },
+        {
+          id: 'a',
+          name: 'Bob',
+          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+        },
+      ],
+    }
+  ],
+  current: null,
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CREATE_EVENT: {
+      return {
+        ...state,
+        events: [...state.events, payload],
+      };
+    }
+
+    case UPDATE_EVENT: {
+      return {
+        ...state,
+        current: payload,
+      };
+    }
+
+    case DELETE_EVENT: {
+      return {
+        ...state,
+        events: state.events.filter((event) => event.id !== payload),
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import eventReducer from './event';
 
-export default combineReducers({});
+export default combineReducers({
+  events: eventReducer,
+});
